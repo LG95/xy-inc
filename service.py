5c14f2a588fdd35565a2b8bc05e9924202ee8573
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-09-09 01:12:01 -0300

    Modified to work with string parameters.

--- service.py
+++ service.py
@@ -9,16 +9,18 @@ class PointOfInterestServices:
 	""" Class implementing a service around PointOfInterest. """
 
 	@staticmethod
-	def initialize():	# type: () -> None
+	def _initialize():	# type: () -> None
 		""" Initialize the database, creating tables not yet present. """
 
 		Database.initialize()
 
 	@staticmethod
-	def add_poi(name, x, y):	# type: (str, int, int) -> None
+	def add_poi(name, x, y):	# type: (str, str, str) -> None
 		""" Store the point at (x, y) named name. """
 
-		Database.add_poi( PointOfInterest(name, x, y) )
+		assert x.isdigit(), x + " is not an integer"
+		assert y.isdigit(), y + " is not an integer"
+		Database.add_poi( PointOfInterest(name, int(x), int(y)) )
 
 	@staticmethod
 	def list_pois():	# type: () -> List[str]
@@ -27,7 +29,10 @@ class PointOfInterestServices:
 		return [poi.name for poi in Database.list_pois()]
 
 	@staticmethod
-	def list_nearby(x, y, d_max):	# type: (int, int, int) -> List[str]
+	def list_nearby(x, y, d_max):	# type: (str, str, str) -> List[str]
 		""" Return a list of the names of points that are at most d_max away from (x, y). """
 
-		return [poi.name for poi in Database.list_nearby_pois(x, y, d_max)]
+		assert x.isdigit(), x + " is not an integer"
+		assert y.isdigit(), y + " is not an integer"
+		assert d_max.isdigit(), d_max + " is not an integer"
+		return [poi.name for poi in Database.list_nearby_pois(int(x), int(y), int(d_max))]
